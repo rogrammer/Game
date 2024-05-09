da2ea6dc58e1a3d1432fc660608df57fb71a13fc
rogrammer <ibahca36@gmail.com>
2024-05-09 23:37:19 +0300

    add 1 ui widget 1 plugin

--- contactus.js
+++ contactus.js
@@ -1,9 +1,35 @@
-$("#submit").button({
-        "ui-button" : "ui-corner-all"
-})
-
- 	
-
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
+
+const countries = [
+"United States",
+"China",
+"India",
+"Indonesia",
+"Pakistan",
+"Brazil",
+"Nigeria",
+"Bangladesh",
+"Russia",
+"Mexico",
+"Japan",
+"Ethiopia",
+"Philippines",
+"Egypt",
+"Vietnam",
+"DR Congo",
+"Turkey",
+"Iran",
+"Germany",
+"Thailand",
+"United Kingdom",
+"France",
+"Italy",
+"Tanzania",
+"South Africa"
+];
+
+$( "#country" ).autocomplete({
+        source: countries
+      });
