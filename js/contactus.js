bb1cc4c24dcc2f8025736f727fa4801641996c6e
GH0ST-TF141 <87570239+GH0ST-TF141@users.noreply.github.com>
2024-05-10 00:43:01 +0300

    adding validator jquery plugins

--- js/contactus.js
+++ js/contactus.js
@@ -33,3 +33,26 @@ const countries = [
 $( "#country" ).autocomplete({
         source: countries
       });
+
+      $(document).ready(function(){
+        $('#contactForm').submit(function(e){
+            e.preventDefault();
+            var name = $('#name').val();
+            var surname = $('#surname').val();
+            var email = $('#email').val();
+            var country = $('#country').val();
+            var birthdate = $('#datepicker').val();
+            var message = $('#message').val();
+
+            // Form validation
+            if(name == '' || surname == '' || email == '' || country == '' || birthdate == '' || message == ''){
+                alert('Please fill in all fields');
+                return;
+            }
+
+            // Submit the form if all fields are filled
+
+            // If submission is successful, show success message
+            $('#successMessage').show();
+        });
+    });      
